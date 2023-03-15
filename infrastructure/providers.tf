50424c0415b7f349f0b139e5e13d77db6d867d79
Eliton <eliton@outlook.com>
2023-03-15 20:34:51 +0100

    gitignore increment

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -9,12 +9,10 @@ terraform {
 
   backend "gcs" {
     bucket      = "terraform-tfstate-dtc-de-project"
-    credentials = "/Users/gadotte/other/dtc-dataengineering-zoomcamp/dtc-de-375519-104e2a094182.json"
   }
 }
 
 provider "google" {
   project     = var.project
   zone        = local.zone
-  credentials = "/Users/gadotte/other/dtc-dataengineering-zoomcamp/dtc-de-375519-104e2a094182.json"
 }
--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,4 +1,9 @@
 # https://github.com/sindresorhus/guides/blob/main/docker-without-sudo.md
 # https://docs.prefect.io/tutorials/docker/
 
-docker pull prefecthq/prefect
+
+gcloud compute scp ~/.ssh/elituhh_id_rsa elitong3dotti@prefect-vm:/home/elitong3dotti/.ssh/
+
+docker compose up -d
+
+credentials = "/Users/gadotte/other/dtc-dataengineering-zoomcamp/dtc-de-375519-104e2a094182.json"
