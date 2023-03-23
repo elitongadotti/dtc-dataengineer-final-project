5e2f2680376ff27a70e9e511694aa8ff8f44633e
Eliton <eliton@outlook.com>
2023-03-23 20:07:12 +0100

    tf file fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,8 +15,8 @@ sudo apt-get -y install \
 
 # copy sensitive files to the VM
 echo "Exporting sensitive files"
-echo ${service_account_content} > ~/default-sa.json
-echo ${ssh_pvt_key} > /$USER/.ssh/ssh_key_default
+echo "${service_account_content}" > ~/default-sa.json
+echo "${ssh_pvt_key}" > /$USER/.ssh/ssh_key_default
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
