f11fa3a759abe40b0f76ef2b9199dbfdc4e5dcef
Eliton D. Gadotti <elituhh@gmail.com>
2023-03-31 15:02:51 +0000

    clean code

--- dbt/models/cota_parlamentar_gold.sql
+++ dbt/models/cota_parlamentar_gold.sql
@@ -6,11 +6,11 @@ select
     round(coalesce(vlrRestituicao, 0), 2) restitution_value,
     round(coalesce(vlrLiquido, 0), 2) net_value,
     cast(datEmissao as datetime) as issue_date,
-    coalesce(txtDescricao, "") bill_description,
-    cast(cast( (IF(cpf = "nan", "0", cpf)) as FLOAT64) as INT64) as id_parliamentarian
+    coalesce(txtDescricao, "") bill_description
+    --cpf as id_parliamentarian
 from 
     {{ source('staging', 'cota_parlamentar_raw') }}
 where 
-    sgPartido is not null 
-    and sgUF is not null
-    and datEmissao is not null and datEmissao != "nan"
+    sgPartido != ""
+    and sgUF != ""
+    and datEmissao != ""
